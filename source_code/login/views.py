41683e6a514593d062e89c10af1da96040a7cfce
sarwar.hossain@g.bracu.ac.bd <sarwar.hossain@g.bracu.ac.bd>
2021-09-23 00:37:39 +0600

    Login Update Complete
    Check password using SQLite Database

--- source_code/login/views.py
+++ source_code/login/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Users
 
 # Create your views here.
 def login1(request):
     return render(request, "login.html")
+
+def checkpass(request):
+    val_user= request.POST['username']
+    val_pass= request.POST['password']
+    x=Users.objects.filter(userid=val_user)
+    if len(x)==0:
+        return render(request, "login2.html");
+    a=Users.objects.get(userid=val_user)
+    if(a.passw==val_pass):
+        return render(request, "inventory.html");
+    else:
+        return render(request, "login2.html");
